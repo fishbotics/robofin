c9be8f6502a612818a0c46591d3470d2111e5475
Adam Fishman <afishman@cs.washington.edu>
2023-04-12 12:14:22 -0700

    Added a fast, deterministic sampler for the robot points

--- robofin/kinematics/cpu.py
+++ robofin/kinematics/cpu.py
@@ -407,10 +407,10 @@ def label(array, lbl):
 
 
 @numba.jit(nopython=True, cache=True)
-def sample_points_on_franka_arm(
+def get_points_on_franka_arm(
     cfg,
     prismatic_joint,
-    num_points,
+    sample,
     panda_link0_points,
     panda_link1_points,
     panda_link2_points,
@@ -440,16 +440,18 @@ def sample_points_on_franka_arm(
         ),
         axis=0,
     )
-    return all_points[
-        np.random.choice(all_points.shape[0], num_points, replace=False), :
-    ]
+    if sample > 0:
+        return all_points[
+            np.random.choice(all_points.shape[0], sample, replace=False), :
+        ]
+    return all_points
 
 
 @numba.jit(nopython=True, cache=True)
-def sample_points_on_franka_eef(
+def get_points_on_franka_eef(
     pose,
     prismatic_joint,
-    num_points,
+    sample,
     panda_hand_points,
     panda_leftfinger_points,
     panda_rightfinger_points,
@@ -496,9 +498,163 @@ def sample_points_on_franka_eef(
         ),
         axis=0,
     )
-    return all_points[
-        np.random.choice(all_points.shape[0], num_points, replace=False), :
-    ]
+    if sample > 0:
+        return all_points[
+            np.random.choice(all_points.shape[0], sample, replace=False), :
+        ]
+    return all_points
+
+
+class FrankaPoints:
+    def __init__(
+        self,
+        num_robot_points,
+        num_eef_points,
+        use_cache=True,
+        with_base_link=True,
+    ):
+        logging.getLogger("trimesh").setLevel("ERROR")
+        self.with_base_link = with_base_link
+        self.num_robot_points = num_robot_points
+        self.num_eef_points = num_eef_points
+
+        if use_cache and self._init_from_cache_():
+            return
+
+        robot = urchin.URDF.load(FrankaRobot.urdf, lazy_load_meshes=True)
+
+        # If we made it all the way here with the use_cache flag set,
+        # then we should be creating new cache files locally
+        self.points = {
+            **self._initialize_robot_points(robot, num_robot_points),
+            **self._initialize_eef_points(robot, num_eef_points),
+        }
+
+        if use_cache:
+            points_to_save = {k: arr for k, arr in self.points.items()}
+            file_name = self._get_cache_file_name_()
+            print(f"Saving new file to cache: {file_name}")
+            np.save(file_name, points_to_save)
+
+    def _initialize_eef_points(self, robot, N):
+        links = [
+            link
+            for link in robot.links
+            if link.name
+            in set(
+                [
+                    "panda_hand",
+                    "panda_rightfinger",
+                    "panda_leftfinger",
+                ]
+            )
+        ]
+        meshes = [
+            trimesh.load(
+                Path(FrankaRobot.urdf).parent / link.visuals[0].geometry.mesh.filename,
+                force="mesh",
+            )
+            for link in links
+        ]
+        areas = [mesh.bounding_box_oriented.area for mesh in meshes]
+        num_points = np.round(N * np.array(areas) / np.sum(areas))
+
+        points = {}
+        for ii, mesh in enumerate(meshes):
+            points[f"eef_{links[ii].name}"] = trimesh.sample.sample_surface(
+                mesh, int(num_points[ii])
+            )[0]
+        return points
+
+    def _initialize_robot_points(self, robot, N):
+        links = [
+            link
+            for link in robot.links
+            if link.name
+            in set(
+                [
+                    "panda_link0",
+                    "panda_link1",
+                    "panda_link2",
+                    "panda_link3",
+                    "panda_link4",
+                    "panda_link5",
+                    "panda_link6",
+                    "panda_link7",
+                    "panda_hand",
+                    "panda_rightfinger",
+                    "panda_leftfinger",
+                ]
+            )
+        ]
+
+        meshes = [
+            trimesh.load(
+                Path(FrankaRobot.urdf).parent / link.visuals[0].geometry.mesh.filename,
+                force="mesh",
+            )
+            for link in links
+        ]
+        areas = [mesh.bounding_box_oriented.area for mesh in meshes]
+        num_points = np.round(N * np.array(areas) / np.sum(areas))
+
+        points = {}
+        for ii, mesh in enumerate(meshes):
+            points[links[ii].name] = trimesh.sample.sample_surface(
+                mesh, int(num_points[ii])
+            )[0]
+        return points
+
+    def _get_cache_file_name_(self):
+        return (
+            FrankaRobot.pointcloud_cache
+            / f"deterministic_point_cloud_{self.num_robot_points}_{self.num_eef_points}.npy"
+        )
+
+    def _init_from_cache_(self):
+        file_name = self._get_cache_file_name_()
+        if not file_name.is_file():
+            return False
+
+        points = np.load(
+            file_name,
+            allow_pickle=True,
+        )
+        self.points = points.item()
+        return True
+
+    def arm(self, cfg, prismatic_joint):
+        return get_points_on_franka_arm(
+            cfg,
+            prismatic_joint,
+            0,
+            self.points["panda_link0"],
+            self.points["panda_link1"],
+            self.points["panda_link2"],
+            self.points["panda_link3"],
+            self.points["panda_link4"],
+            self.points["panda_link5"],
+            self.points["panda_link6"],
+            self.points["panda_link7"],
+            self.points["panda_hand"],
+            self.points["panda_leftfinger"],
+            self.points["panda_rightfinger"],
+        )
+
+    def end_effector(self, pose, prismatic_joint, frame="right_gripper"):
+        """
+        An internal method--separated so that the public facing method can
+        choose whether or not to have gradients
+        """
+        return get_points_on_franka_eef(
+            pose,
+            prismatic_joint,
+            0,
+            self.points["eef_panda_hand"],
+            self.points["eef_panda_leftfinger"],
+            self.points["eef_panda_rightfinger"],
+            frame,
+        )
 
 
 class FrankaCPUSampler:
@@ -579,7 +735,7 @@ class FrankaCPUSampler:
         return True
 
     def sample_arm(self, cfg, prismatic_joint, num_points):
-        return sample_points_on_franka_arm(
+        return get_points_on_franka_arm(
             cfg,
             prismatic_joint,
             num_points,
@@ -603,7 +759,7 @@ class FrankaCPUSampler:
         An internal method--separated so that the public facing method can
         choose whether or not to have gradients
         """
-        return sample_points_on_franka_eef(
+        return get_points_on_franka_eef(
             pose,
             prismatic_joint,
             num_points,
