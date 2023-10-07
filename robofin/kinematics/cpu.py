d20e558f49dbfb5c09b8faad73e475e5ab62c8ae
Adam Fishman <afishman@cs.washington.edu>
2023-10-07 08:17:53 -0700

    Fixed bugs in the cpu sampler implementations

--- robofin/kinematics/cpu.py
+++ robofin/kinematics/cpu.py
@@ -1,11 +1,12 @@
-import numpy as np
 import logging
-import urchin
-from robofin.robots import FrankaRobot
-
-import trimesh
 from pathlib import Path
-from robofin.kinematics.numba import get_points_on_franka_eef, get_points_on_franka_arm
+
+import numpy as np
+import trimesh
+import urchin
+
+from robofin.kinematics.numba import get_points_on_franka_arm, get_points_on_franka_eef
+from robofin.robots import FrankaRobot
 
 
 class FrankaPoints:
@@ -28,18 +29,33 @@ class FrankaPoints:
 
         # If we made it all the way here with the use_cache flag set,
         # then we should be creating new cache files locally
+        link_points, link_normals = self._initialize_robot_points(
+            robot, num_robot_points
+        )
+        eef_points, eef_normals = self._initialize_eef_points_and_normals(
+            robot, num_eef_points
+        )
         self.points = {
-            **self._initialize_robot_points(robot, num_robot_points),
-            **self._initialize_eef_points(robot, num_eef_points),
+            **link_points,
+            **eef_points,
+        }
+        self.normals = {
+            **link_normals,
+            **eef_normals,
         }
 
         if use_cache:
-            points_to_save = {k: arr for k, arr in self.points.items()}
+            points_to_save = {}
+            for key in self.points.items():
+                assert key in self.normals
+                pc = self.points[key]
+                normals = self.normals[key]
+                points_to_save[key] = {"pc": pc, "normals": normals}
             file_name = self._get_cache_file_name_()
             print(f"Saving new file to cache: {file_name}")
             np.save(file_name, points_to_save)
 
-    def _initialize_eef_points(self, robot, N):
+    def _initialize_eef_points_and_normals(self, robot, N):
         links = [
             link
             for link in robot.links
@@ -63,11 +79,16 @@ class FrankaPoints:
         num_points = np.round(N * np.array(areas) / np.sum(areas))
 
         points = {}
+        normals = {}
         for ii, mesh in enumerate(meshes):
-            points[f"eef_{links[ii].name}"] = trimesh.sample.sample_surface(
+            link_pc, face_indices = trimesh.sample.sample_surface(
                 mesh, int(num_points[ii])
-            )[0]
-        return points
+            )
+            points[f"eef_{links[ii].name}"] = link_pc
+            normals[f"eef_{links[ii].name}"] = self._init_normals(
+                mesh, link_pc, face_indices
+            )
+        return points, normals
 
     def _initialize_robot_points(self, robot, N):
         links = [
@@ -102,11 +123,29 @@ class FrankaPoints:
         num_points = np.round(N * np.array(areas) / np.sum(areas))
 
         points = {}
+        normals = {}
         for ii, mesh in enumerate(meshes):
-            points[links[ii].name] = trimesh.sample.sample_surface(
+            link_pc, face_indices = trimesh.sample.sample_surface(
                 mesh, int(num_points[ii])
             )[0]
-        return points
+            points[links[ii].name] = link_pc
+            normals[f"eef_{links[ii].name}"] = self._init_normals(
+                mesh, link_pc, face_indices
+            )
+        return points, normals
+
+    def _init_normals(self, mesh, pc, face_indices):
+        bary = trimesh.triangles.points_to_barycentric(
+            triangles=mesh.triangles[face_indices], points=pc
+        )
+        # interpolate vertex normals from barycentric coordinates
+        normals = trimesh.unitize(
+            (
+                mesh.vertex_normals[mesh.faces[face_indices]]
+                * trimesh.unitize(bary).reshape((-1, 3, 1))
+            ).sum(axis=1)
+        )
+        return normals
 
     def _get_cache_file_name_(self):
         return (
@@ -123,7 +162,8 @@ class FrankaPoints:
             file_name,
             allow_pickle=True,
         )
-        self.points = points.item()
+        self.points = {key: v["pc"] for key, v in points.item().items()}
+        self.normals = {key: v["normals"] for key, v in points.item().items()}
         return True
 
     def arm(self, cfg, prismatic_joint):
@@ -173,9 +213,9 @@ class FrankaCPUSampler:
 
         robot = urchin.URDF.load(FrankaRobot.urdf, lazy_load_meshes=True)
         self.links = [
-            l
-            for l in robot.links
-            if l.name
+            link
+            for link in robot.links
+            if link.name
             in set(
                 [
                     "panda_link0",
@@ -197,30 +237,47 @@ class FrankaCPUSampler:
 
         meshes = [
             trimesh.load(
-                Path(FrankaRobot.urdf).parent / l.visuals[0].geometry.mesh.filename,
+                Path(FrankaRobot.urdf).parent / link.visuals[0].geometry.mesh.filename,
                 force="mesh",
             )
-            for l in self.links
+            for link in self.links
         ]
         areas = [mesh.bounding_box_oriented.area for mesh in meshes]
         num_points = np.round(max_points * np.array(areas) / np.sum(areas))
 
         self.points = {}
         for ii, mesh in enumerate(meshes):
-            self.points[self.links[ii].name] = trimesh.sample.sample_surface(
-                mesh, int(num_points[ii])
-            )[0]
-
+            pc, face_indices = trimesh.sample.sample_surface(mesh, int(num_points[ii]))
+            self.points[self.links[ii].name] = pc
+            self.normals[self.links[ii].name] = self._init_normals(
+                mesh, pc, face_indices
+            )
         # If we made it all the way here with the use_cache flag set,
         # then we should be creating new cache files locally
 
         if use_cache:
-            points_to_save = {
-                k: tensor.squeeze(0).cpu().numpy() for k, tensor in self.points.items()
-            }
+            points_to_save = {}
+            for key in self.points.items():
+                assert key in self.normals
+                pc = self.points[key]
+                normals = self.normals[key]
+                points_to_save[key] = {"pc": pc, "normals": normals}
             file_name = self._get_cache_file_name_()
             print(f"Saving new file to cache: {file_name}")
             np.save(file_name, points_to_save)
+
+    def _init_normals(self, mesh, pc, face_indices):
+        bary = trimesh.triangles.points_to_barycentric(
+            triangles=mesh.triangles[face_indices], points=pc
+        )
+        # interpolate vertex normals from barycentric coordinates
+        normals = trimesh.unitize(
+            (
+                mesh.vertex_normals[mesh.faces[face_indices]]
+                * trimesh.unitize(bary).reshape((-1, 3, 1))
+            ).sum(axis=1)
+        )
+        return normals
 
     def _get_cache_file_name_(self):
         return FrankaRobot.pointcloud_cache / f"full_point_cloud_{self.max_points}.npy"
@@ -234,7 +291,8 @@ class FrankaCPUSampler:
             file_name,
             allow_pickle=True,
         )
-        self.points = points.item()
+        self.points = {key: v["pc"] for key, v in points.item().items()}
+        self.normals = {key: v["normals"] for key, v in points.item().items()}
         return True
 
     def sample_arm(self, cfg, prismatic_joint, num_points):
